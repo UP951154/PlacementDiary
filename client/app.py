80738a0e2e79d555ef6055422ef21f986068bae7
up951154 <up951154@myport.ac.uk>
2023-11-22 16:38:31 +0000

    added process_data function on flask app

--- client/app.py
+++ client/app.py
@@ -6,11 +6,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    heading = request.args.get('page_heading')
-    date = request.args.get('diary_header')
-    
-    print('date: ', date)
-    print('heading: ', heading)
+    print('Flask app running')
     return render_template('index.html')
+
+
+@app.route('/process_data', methods=['POST', 'GET'])
+def process_data():
+    print('Processing data')
+    try:
+        data = request.get_json()
+
+        date_heading = data.get('date_heading')
+        
+        print(f'Data retrieved: {data}')
+        return data
+
+    except Exception as e:
+        
+        return f'Error processing data: {str(e)}', 500
+
 if __name__ == '__main__':
     app.run(debug=True)
