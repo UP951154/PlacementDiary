75b9282284f5530337ba982bf11ca1ff786fc8c5
up951154 <up951154@myport.ac.uk>
2023-11-23 22:41:09 +0000

    click on day and get sent to data page

--- client/static/js/index.js
+++ client/static/js/index.js
@@ -83,10 +83,14 @@ function displayCalendar(month, year) {
       // Add selected class to selected currentDay
       event.target.classList.add('selected');
 
-      // Update diary entry for selected date
-      const date = new Date(year, month, currentDay);
+         // Construct the URL with query parameters
+         const url = 'data?' +
+         'year=' + encodeURIComponent(year) +
+         '&month=' + encodeURIComponent(month) +
+         '&day=' + encodeURIComponent(currentDay);
 
-      displayDiaryEntry(date);
+     // Redirect to the new page
+     window.location.href = url;
     }
   });
 
@@ -139,68 +143,8 @@ function getMonthName(month) {
   return monthNames[month];
 }
 
-function displayDiaryEntry(date) {
-  diaryEntryElement.innerHTML = '';
-  const currentDay = date.getDate();
-
-  // Format date
-  var year = date.getFullYear().toString(); // Get the last two digits of the year
-  var month = ('0' + (date.getMonth() + 1)).slice(-2); // Add leading zero if needed
-  var day = ('0' + date.getDate()).slice(-2); // Add leading zero if needed
-  var formattedDate = year + '-' + month + '-' + day;
-
-  
-  // Date
-  const header = document.createElement('h2');
-  header.id = 'diary_header';
-  // header.innerHTML = formattedDate;
-  header.innerHTML = date.toDateString();
-  
-  diaryEntryElement.appendChild(header);
-
-  // Input
-  createInput('work_description', '', 'Work carried out');
-  createInput('experience_description', '', 'Experience gained and skills developed');
-  createInput('competency', '', 'Competency');
-
-  // Buttons
-  createButton('submitbtn', '<i class="fa fa-check"></i>', function () {
-    sendDataToFlask(formattedDate);
-  });
-  createButton('removebtn', '<i class="fa fa-trash"></i>',function () {
-    deleteData(formattedDate);
-  });
-  createButton('clearbtn', '<i class="fa fa-times"></i>', function(){
-    clearCalendar();
-  });
-
-  retrieveData(formattedDate)
-  
-}
 
 // Utility functions
-
-function createButton(id, content, onclickFunc, onclickParams) {
-  const btn = document.createElement('button');
-  btn.id = id;
-  btn.innerHTML = content;
-  btn.addEventListener('click', function () {
-    if (Array.isArray(onclickParams)) {
-      onclickFunc(...onclickParams);
-    } else {
-      onclickFunc(onclickParams);
-    }
-  });
-  diaryEntryElement.appendChild(btn);
-}
-
-function createInput(id, content, placeholder) {
-  const input = document.createElement('textarea');
-  input.id = id;
-  input.textContent = content;
-  input.placeholder = placeholder;
-  diaryEntryElement.appendChild(input);
-}
 
 function openNav() {
   document.getElementById('mySidenav').style.width = '250px';
@@ -208,118 +152,6 @@ function openNav() {
 
 function closeNav() {
   document.getElementById('mySidenav').style.width = '0';
-}
-
-// APIS
-
-function sendDataToFlask(date) {
-  const data = {
-    page_title: document.getElementById('page_heading').innerHTML,
-    date: date,
-    work_description: document.getElementById('work_description').value,
-    experience_description: document.getElementById('experience_description').value,
-    competency: document.getElementById('competency').value,
-  };
-
-  fetch('/insert_data', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then(result => {
-      // Handle the result if needed
-    })
-    .catch(error => {
-      console.error('Error:', error.message); // Log the error message
-    });
-}
-
-function retrieveData(date){
-  date = date
-  fetch('/retrieve_data', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams({
-          'date': date,  // Replace with the actual date or get it dynamically
-      }),
-  })
-  .then(response => {
-      
-      if (response.ok && response.headers.get('content-length') !== '0') {
-        return response.json();
-      } else {
-        return '';  // Return an empty string if the response is empty
-      }
-    })
-  .then(data => {
-      // Handle the retrieved data here
-      if (data.length >= 3) {
-        document.getElementById('work_description').value = data[0];
-        document.getElementById('experience_description').value = data[1];
-        document.getElementById('competency').value = data[2];
-      } else {
-        document.getElementById('work_description').value = '';
-        document.getElementById('experience_description').value = '';
-        document.getElementById('competency').value = '';
-      }
-
-  })
-  .catch(error => {
-      console.error('Error:', error);
-  });
-}
-
-function deleteData(date){
-      
-  const data = {
-
-    date: date,
-
-  };
-
-  fetch('/delete_data', {
-    method: 'POST',
-    headers: {
-  'Content-Type': 'application/json',
-  },
-    body: JSON.stringify(data),
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json();
-})
-  .then(data => {   
-  })
-}
-
-function clearCalendar() {
-  // Assuming you have a Flask route to handle the clear_calendar function
-  fetch('/clear_calendar')
-      .then(response => {
-          if (!response.ok) {
-              throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-          return response.json(); // assuming server returns JSON
-      })
-      .then(data => {
-          console.log(data);
-          location.reload(true); // log the response from the server
-      })
-      .catch(error => {
-          console.error('Error:', error);
-      });
 }
 
 
